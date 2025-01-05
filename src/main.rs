2f3727dc6128fddda159eb0227c449d7b0470f86
Binh Nguyen <b.nguyen.sn@gmail.com>
2025-01-05 01:37:16 +0000

    add exercise 401 and answers

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,24 @@ mod common {
 }
 
 mod mod_03 {
-    pub mod ex_01_struct;
-    pub mod ex_02_validation;
-    pub mod ex_03_modules;
-    pub mod ex_04_visibility;
-    pub mod ex_05_encapsulation;
-    pub mod ex_06_ownership;
-    pub mod ex_07_setters;
-    pub mod ex_08_stack;
-    pub mod ex_09_heap;
-    pub mod ex_10_references;
-
+    mod ex_01_struct;
+    mod ex_02_validation;
+    mod ex_03_modules;
+    mod ex_04_visibility;
+    mod ex_05_encapsulation;
+    mod ex_06_ownership;
+    mod ex_07_setters;
+    mod ex_08_stack;
+    mod ex_09_heap;
+    mod ex_10_references;
     mod outro {
-        pub mod ex_12_outro;
-        pub mod ex_12_outro_test;
+        mod ex_12_outro;
+        mod ex_12_outro_test;
     }
+}
+
+mod mod_04 {
+    mod ex_401_trait;
 }
 
 fn main() {
