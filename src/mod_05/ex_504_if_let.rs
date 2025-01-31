3aca28d0cff1b8f1f653a9309085d78de0b504ec
Binh Nguyen <b.nguyen.sn@gmail.com>
2025-01-31 00:11:08 +0000

    update exercise 504

--- src/mod_05/ex_504_if_let.rs
+++ src/mod_05/ex_504_if_let.rs
@@ -11,8 +11,8 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        if let Shape::Circle { radius } = &self {
-            radius * PI
+        if let Shape::Circle { radius } = self {
+            *radius
         } else {
             panic!("Not a circle")
         }
